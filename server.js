37c3a8effa42202ddd0f5ea290d194cbacf8873d
Technosophos <technosophos@gmail.com>
2012-10-01 13:10:27 -0500

    Updated Server with error routes, PUT.

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var initialContext = new pronto.Context({
 
 register
   // Set up the logger
-  .logger(pronto.logging.ConsoleLogger)
+  .logger(pronto.logging.ConsoleLogger, {colors: true})
   // Operations common across the board.
   .route('@bootstrap')
     .does(webdav.http.GetNormalizedPath, 'path')
@@ -71,7 +71,19 @@ register
       .using('code', 404).from('cxt:delete')
 
   .route('PUT')
+    .does(pronto.commands.BufferRequest, 'input')
     .includes('@bootstrap')
+    .does(webdav.backend.LoadResource, 'resource')
+      .using('resourceBridge').from('cxt:bridge')
+      .using('name').from('cxt:path')
+    .does(webdav.http.HandlePut, 'put')
+      .using('resourceBridge').from('cxt:bridge')
+      .using('path').from('cxt:path')
+      .using('resource').from('cxt:resource')
+      .using('readStream').from('cxt:input')
+    .does(pronto.commands.HTTPResponse)
+      .using('headers', {}).from('cxt:httpHeaders')
+      .using('code', 404).from('cxt:put')
 
   .route('PROPFIND')
     .includes('@bootstrap')
@@ -89,6 +101,23 @@ register
   // ================================================================
   // Error Routes
   // ================================================================
+  .route('@304')
+    // 304 should never have a body. It is up to the app to put
+    // one of Date, ETag, Expires, Vary, and so on.
+    .does(pronto.commands.HTTPResponse)
+      .using('headers', {}).from('cxt:httpHeaders')
+      .using('code', 304)
+
+  .route('@403')
+    .does(pronto.commands.SPrintF, 'body')
+      .using('format', pronto.commands.SPrintF.HTML5)
+      .using('1', 'Forbidden')
+      .using('2', 'Forbidden')
+    .does(pronto.commands.HTTPResponse)
+      .using('headers', {}) //.from('cxt:httpHeaders')
+      .using('code', 403)
+      .using('body').from('cxt:body')
+
   .route('@404')
     .does(pronto.commands.SPrintF, 'body')
       .using('format', pronto.commands.SPrintF.HTML5)
@@ -98,6 +127,27 @@ register
       .using('headers', {}) //.from('cxt:httpHeaders')
       .using('code', 404)
       .using('body').from('cxt:body')
+
+  .route('@409')
+    .does(pronto.commands.SPrintF, 'body')
+      .using('format', pronto.commands.SPrintF.HTML5)
+      .using('1', 'Conflict')
+      .using('2', 'Conflict').from('cxt:body')
+    .does(pronto.commands.HTTPResponse)
+      .using('headers', {}) //.from('cxt:httpHeaders')
+      .using('code', 409)
+      .using('body').from('cxt:body')
+
+  .route('@412')
+    .does(pronto.commands.SPrintF, 'body')
+      .using('format', pronto.commands.SPrintF.HTML5)
+      .using('1', 'Precondition Failed')
+      .using('2', 'Precondition Failed').from('cxt:body')
+    .does(pronto.commands.HTTPResponse)
+      .using('headers', {}) //.from('cxt:httpHeaders')
+      .using('code', 412)
+      .using('body').from('cxt:body')
+
   .route('@500')
     .does(pronto.commands.SPrintF, 'body')
       .using('format', pronto.commands.SPrintF.HTML5)
@@ -107,6 +157,7 @@ register
       .using('headers', {}) //.from('cxt:httpHeaders')
       .using('code', 500)
       .using('body').from('cxt:body')
+
   .route('@501')
     .does(pronto.commands.SPrintF, 'body')
       .using('format', pronto.commands.SPrintF.HTML5)
