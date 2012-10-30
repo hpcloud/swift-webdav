b568a03a7f2e439a52e81e78317dde1b263a289b
Technosophos <technosophos@gmail.com>
2012-10-30 17:19:57 -0500

    Check for collection before creating collection.

--- server.js
+++ server.js
@@ -166,6 +166,7 @@ register
   // 201 or 207, with our without a body.
   .route('MKCOL')
     .includes('@bootstrap')
+    .includes('@loadresource')
     .does(webdav.xml.ParseXML, 'xml')
     .does(webdav.http.HandleMkcol, 'mkcol')
       .using('path').from('cxt:path')
